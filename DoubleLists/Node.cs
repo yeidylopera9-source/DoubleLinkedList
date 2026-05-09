28fb8d2b0f91749f129d30ed64a1cc81fea8c133
Yeidy Lopera <yeidylopera9@gmail.com>
2026-05-09 18:42:29 -0500

    add

---
+++ DoubleLists/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace DoubleList;
+
+public class Node<T>
+{
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+        Previous = null;
+    }
+
+    public T? Data { get; set; }
+    public Node<T>? Next { get; set; }
+    public Node<T>? Previous { get; set; }
+}
